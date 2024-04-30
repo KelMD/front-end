a4bf2f45af3b9b7996fec867fc683b701616c0fa
Kellie Dickerson <Kellie.Dickerson@LibertyMutual.com>
2024-04-30 12:56:19 -0700

    Login capability is now available

--- src/NewsReader.js
+++ src/NewsReader.js
@@ -3,13 +3,16 @@ import { exampleQuery ,exampleData } from './data';
 import { QueryForm } from './QueryForm';
 import { Articles } from './Articles';
 import { useState, useEffect } from 'react';
-
+import { LoginForm } from './LoginForm';
 
 export function NewsReader() {
   const [query, setQuery] = useState(exampleQuery); // latest query send to newsapi
   const [data, setData] = useState(exampleData);   // current data returned from newsapi
+  const [currentUser, setCurrentUser] = useState(null);
+  const [credentials, setCredentials] = useState({ user: "", password: "" })  
   const urlNews="/news";
   const urlQueries = "/queries";
+  const urlUsersAuth = "/users/authenticate";
   const [savedQueries, setSavedQueries] = useState([{ ...exampleQuery }]);
   const [queryFormObject, setQueryFormObject] = useState({ ...exampleQuery });
 
@@ -17,6 +20,32 @@ export function NewsReader() {
     getNews(query);
   }, [query])
   useEffect(() => {getQueryList();}, [])
+
+  async function login() {
+    if (currentUser !== null) {
+      // logout
+      setCurrentUser(null);
+    } else {
+      // login
+      try {
+        const response = await fetch(urlUsersAuth, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(credentials),
+        });
+        if (response.status === 200) {
+          setCurrentUser({ ...credentials });
+          setCredentials({ user: "", password: "" });
+        } else {
+          alert("Error during authentication! " + credentials.user + "/" + credentials.password);
+          setCurrentUser(null);
+        }
+      } catch (error) {
+        console.error('Error authenticating user:', error);
+        setCurrentUser(null);
+      }
+    }
+  }
 
   async function getQueryList() {
     try {
@@ -30,6 +59,7 @@ export function NewsReader() {
     console.error('Error fetching news:', error);
     }
    } 
+  
    async function saveQueryList(savedQueries) {
     try {
     const response = await fetch(urlQueries, {
@@ -89,6 +119,10 @@ export function NewsReader() {
 
   return (
     <div>
+      <LoginForm login={login} 
+        credentials={credentials} 
+        currentUser={currentUser} 
+        setCredentials={setCredentials} />
       <div >
         <section className="parent" >
           <div className="box">
