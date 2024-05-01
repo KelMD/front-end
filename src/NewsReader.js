da744d20ed73193273f11e1ff5445fb69336db24
Kellie Dickerson <Kellie.Dickerson@LibertyMutual.com>
2024-05-01 10:10:17 -0700

    Login restricted features added

--- src/NewsReader.js
+++ src/NewsReader.js
@@ -80,8 +80,27 @@ export function NewsReader() {
     setQueryFormObject(selectedQuery);
     setQuery(selectedQuery);
   }
+   
+  function currentUserMatches(user) {
+    if (currentUser) {
+    if (currentUser.user) {
+    if (currentUser.user === user) {
+    return true;
+    }
+    }
+    }
+    return false;
+    }
 
   function onFormSubmit(queryObject) {
+    if (currentUser === null){
+      alert("Log in if you want to create new queries!")
+      return;
+      }
+    if (savedQueries.length >= 3 && currentUserMatches("guest")) {
+      alert("guest users cannot submit new queries once saved query count is 3 or greater!")
+      return;
+      }
     let newSavedQueries = [];
     newSavedQueries.push(queryObject);
     for (let query of savedQueries) {
@@ -103,7 +122,7 @@ export function NewsReader() {
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(queryObject),
         });
-  
+
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -128,6 +147,7 @@ export function NewsReader() {
           <div className="box">
             <span className='title'>Query Form</span>
             <QueryForm
+              currentUser={currentUser}
               setFormObject={setQueryFormObject}
               formObject={queryFormObject}
               submitToParent={onFormSubmit} />
